d07a681cbcc9c342e5ab57567a8cbdb9205aa285
p0isonedpanda <daniel.coady@outlook.com.au>
2017-10-12 11:38:03 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace PacketAssembler
+{
+    class Program
+    {
+        static List<string> packets = new List<string>();
+
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                String packetInput = sr.ReadToEnd();
+                packets = packetInput.Split("\n").ToList();
+            }
+
+            packets = new List<string>(packets.OrderBy(x => Int32.Parse(x.Substring(0, 4))));
+            
+            string packetOutput = "";
+            for (int i = 0; i < packets.Count; i++)
+            {
+                packetOutput += packets[i] + "\n";
+            }
+            Console.WriteLine(packetOutput);
+        }
+    }
+}
